8a4c59b25f974938a1bc093b103fa280ae1fe72d
Unsar Malik <unsar.malik24@gmail.com>
2019-04-12 00:58:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -83,8 +84,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
 end
 
 def changing_alan
@@ -105,8 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
